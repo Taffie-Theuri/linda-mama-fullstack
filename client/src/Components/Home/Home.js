8dcd3a31f0feca66c7cd286a3cce49f74335d12e
Taffie Theuri <taffietheuri4@gmail.com>
2023-08-10 22:41:17 +0300

    add link to signup page on home page as a button

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 
 import React from "react";
+import { Link } from 'react-router-dom';
 import BannerBackground from "./Assets/home-banner-background.png";
 import { FiArrowRight } from "react-icons/fi";
 import './Home.css'
@@ -22,9 +23,9 @@ const Home = () => {
           <p className="primary-text">
             Sign up to have a stress free and well-informed pregnancy experience.
           </p>
-          <button className="secondary-button">
-            Sign Up<FiArrowRight />{" "}
-          </button>
+          <Link to="/login" className="secondary-button">
+        Sign Up<FiArrowRight />{" "}
+      </Link>
         </div>
       </div>
       <Work/>
